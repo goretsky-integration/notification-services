4ac14ba21c8b46ec8a7f591d9eb640e9694db193
Eldos <eldos.baktybekov@gmail.com>
2022-11-26 21:56:07 +0600

    :sparkles: Add storages

---
+++ src/storages.py
@@ -0,0 +1,53 @@
+import abc
+import json
+import pathlib
+import uuid
+
+__all__ = (
+    'IngredientStopSaleIDsStorage',
+)
+
+
+class BaseStorage(abc.ABC):
+
+    def __init__(self, storage_path: pathlib.Path):
+        self._storage_path = storage_path
+        self._data = None
+
+    def __enter__(self):
+        self.load()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.save()
+
+    @abc.abstractmethod
+    def load(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def save(self) -> None:
+        pass
+
+
+class IngredientStopSaleIDsStorage(BaseStorage):
+
+    def load(self) -> None:
+        if not self._storage_path.exists():
+            self._storage_path.write_text('[]')
+        with open(self._storage_path) as file:
+            self._data = {uuid.UUID(stop_sale_uuid) for stop_sale_uuid in json.load(file)}
+
+    def save(self) -> None:
+        uuids = [stop_sale_uuid.hex for stop_sale_uuid in self._data]
+        with open(self._storage_path, 'w') as file:
+            json.dump(uuids, file)
+
+    def is_exist(self, stop_sale_uuid: uuid.UUID) -> bool:
+        return stop_sale_uuid in self._data
+
+    def add_uuid(self, stop_sale_uuid: uuid.UUID) -> None:
+        self._data.add(stop_sale_uuid)
+
+    def clear(self) -> None:
+        self._data = set()
