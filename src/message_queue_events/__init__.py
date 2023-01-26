17b9c29b5eb509217526e8b5489e1dc69bef85c0
Eldos <eldos.baktybekov@gmail.com>
2023-01-26 18:38:42 +0600

    :construction: GI-5 Add stop sales event messages

--- src/message_queue_events/__init__.py
+++ src/message_queue_events/__init__.py
@@ -1,3 +1,6 @@
+from .cheated_phone_number import *
 from .daily_ingredient_stop import *
+from .stop_sale_by_channel import *
+from .stop_sale_by_ingredient import *
 from .stop_sale_by_sector import *
 from .stop_sale_by_street import *
---
+++ src/message_queue_events/stop_sale_by_channel.py
@@ -0,0 +1,23 @@
+import models
+from message_queue_events.base import MessageQueueEvent
+
+__all__ = ('StopSaleByChannelEvent',)
+
+
+class StopSaleByChannelEvent(MessageQueueEvent):
+
+    def __init__(self, unit_id: int, stop_sale: models.StopSaleBySalesChannel):
+        self.__unit_id = unit_id
+        self.__stop_sale = stop_sale
+
+    def get_data(self):
+        return {
+            'unit_id': self.__unit_id,
+            'type': 'PIZZERIA_STOP_SALES',
+            'payload': {
+                'unit_name': self.__stop_sale.unit_name,
+                'started_at': self.__stop_sale.started_at,
+                'sales_channel_name': self.__stop_sale.sales_channel_name,
+                'reason': self.__stop_sale.reason,
+            },
+        }
