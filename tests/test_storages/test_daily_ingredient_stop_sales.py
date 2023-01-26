750aa41377bc104d3fe7a797ba09d0f512ea7108
Eldos <eldos.baktybekov@gmail.com>
2023-01-26 18:02:53 +0600

    :white_check_mark: GI-5 Refactor tests

--- tests/test_storages/test_daily_ingredient_stop_sales.py
+++ tests/test_storages/test_daily_ingredient_stop_sales.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 
 import pytest
 
@@ -6,22 +7,22 @@ from services.storages import DailyIngredientStopSalesStorage
 
 
 @pytest.mark.parametrize(
-    'unit_name, ingredient_name',
+    'stop_sale_id',
     [
-        ('Москва 4-1', 'Тесто-35'),
-        ('Москва 4-1', 'Тесто-25'),
-        ('Москва 4-1', 'Тесто-45'),
-        ('Москва 4-2', 'Тесто-35'),
-        ('Москва 4-3', 'Тесто-25'),
+        uuid4(),
+        uuid4(),
+        uuid4(),
+        uuid4(),
+        uuid4(),
     ]
 )
-def test_insert_and_exists_stop_sales(unit_name, ingredient_name):
+def test_insert_and_exists_stop_sales(stop_sale_id):
     storage_file_path = './daily_ingredient_stop_sales.db'
     with DailyIngredientStopSalesStorage(storage_file_path) as storage:
         try:
-            assert not storage.is_exist(unit_name, ingredient_name)
-            storage.insert(unit_name, ingredient_name)
-            assert storage.is_exist(unit_name, ingredient_name)
+            assert not storage.is_exist(stop_sale_id)
+            storage.insert(stop_sale_id)
+            assert storage.is_exist(stop_sale_id)
         finally:
             storage.clear_all()
     os.remove(storage_file_path)
