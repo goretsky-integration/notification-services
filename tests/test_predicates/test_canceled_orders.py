0f1c2bd2f7ea60752bc90c2927f79a3ea492a243
Eldos <eldos.baktybekov@gmail.com>
2023-01-29 10:35:54 +0600

    :white_check_mark: GI-5 Add tests

---
+++ tests/test_predicates/test_canceled_orders.py
@@ -0,0 +1,43 @@
+import pytest
+from faker import Faker
+
+import models
+from filters import predicates
+
+faker = Faker()
+
+
+@pytest.mark.parametrize(
+    'order',
+    [
+        models.CanceledOrder(
+            unit_name=faker.name(),
+            created_at=faker.date_time(),
+            receipt_printed_at=faker.date_time(),
+            number=faker.name(),
+            type=faker.name(),
+            price=100,
+            uuid=faker.uuid4(cast_to=None),
+        ) for _ in range(10)
+    ]
+)
+def test_orders_have_printed_receipt(order):
+    assert predicates.has_printed_receipt(order) == True
+
+
+@pytest.mark.parametrize(
+    'order',
+    [
+        models.CanceledOrder(
+            unit_name=faker.name(),
+            created_at=faker.date_time(),
+            receipt_printed_at=None,
+            number=faker.name(),
+            type=faker.name(),
+            price=100,
+            uuid=faker.uuid4(cast_to=None),
+        ) for _ in range(10)
+    ]
+)
+def test_orders_without_printed_receipt(order):
+    assert predicates.has_printed_receipt(order) == False
