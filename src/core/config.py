9e4fc2077fdf39a1e2061b8fe1059bb5468d33a5
Eldos <eldos.baktybekov@gmail.com>
2023-01-29 22:45:52 +0600

    :wrench: GI-5 Add logging config

--- src/core/config.py
+++ src/core/config.py
@@ -36,8 +36,15 @@ class CheatedOrdersConfig:
 
 
 @dataclass(frozen=True, slots=True)
+class LoggingConfig:
+    level: str
+    file_path: str
+
+
+@dataclass(frozen=True, slots=True)
 class Config:
     country_code: str
+    logging: LoggingConfig
     message_queue: MessageQueueConfig
     partial_ingredients: PartialIngredientStopSalesConfig
     cheated_orders: CheatedOrdersConfig
@@ -49,6 +56,10 @@ def load_config(config_file_path: str | pathlib.Path) -> Config:
         config = tomllib.load(file)
     return Config(
         country_code=config['country_code'],
+        logging=LoggingConfig(
+            level=config['logging']['level'],
+            file_path=config['logging']['file_path'],
+        ),
         api=APIConfig(
             auth_api_base_url=config['api']['auth_api_url'],
             dodo_api_base_url=config['api']['dodo_api_url'],
