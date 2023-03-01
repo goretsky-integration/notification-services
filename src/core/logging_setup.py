4e6d5d479729d85ae6cf94364b57d4cd6148eb73
Eldos <eldos.baktybekov@gmail.com>
2023-03-01 09:41:12 +0600

    :loud_sound: Change logging setup (#GI-27)

--- src/core/logging_setup.py
+++ src/core/logging_setup.py
@@ -1,8 +1,15 @@
-import logging
+import logging.handlers
 import pathlib
 
 __all__ = ('setup_logging',)
 
 
 def setup_logging(*, loglevel: str | int = logging.WARNING, logfile_path: str | pathlib.Path | None = None) -> None:
-    logging.basicConfig(filename=logfile_path, level=loglevel)
+    logging.basicConfig(
+        level=loglevel,
+        format='%(asctime)s\t| %(levelname)s\t | %(message)s',
+        handlers=(
+            logging.FileHandler(logfile_path),
+            logging.StreamHandler(),
+        )
+    )
