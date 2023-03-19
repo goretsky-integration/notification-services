4eb3ff0ddd186c1c2ba9c9e8c0f529ab98f5cea2
Eldos <eldos.baktybekov@gmail.com>
2023-03-19 09:44:09 +0600

    :white_check_mark: Add config fixtures (#42)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
+from fixtures.config import expected_config, config_string
 from fixtures.external_api_services import auth_api, http_client
+
+__all__ = (
+    'auth_api',
+    'http_client',
+    'expected_config',
+    'config_string',
+)
