9a8c8d561ad7007340b7ef9e29b06219bcc52927
Eldos <eldos.baktybekov@gmail.com>
2023-04-01 22:24:24 +0600

    :alien: Database API changes (#GI-51)

--- src/services/external_dodo_api/database_api.py
+++ src/services/external_dodo_api/database_api.py
@@ -9,6 +9,21 @@ __all__ = ('DatabaseAPI',)
 class DatabaseAPI(APIService):
 
     def get_units(self) -> tuple[models.Unit, ...]:
-        response = self._client.get('/units/')
-        response_data = response.json()
-        return parse_obj_as(tuple[models.Unit, ...], response_data)
+        request_query_params = {
+            'limit': 100,
+            'offset': 0,
+        }
+        all_units: list[dict] = []
+        while True:
+            response = self._client.get('/units/', params=request_query_params)
+            response_data = response.json()
+
+            if response_data['units']:
+                all_units += response_data['units']
+
+            if response_data['is_end_of_list_reached']:
+                break
+
+            request_query_params['offset'] += request_query_params['limit']
+
+        return parse_obj_as(tuple[models.Unit, ...], all_units)
